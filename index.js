24da07663e105aaa90ee837492cf2d2f673d316a
re5et <re5etsmyth@gmail.com>
2014-03-27 16:44:57 -0700

    Fix some missing semicolons

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('rconsole')
+require('rconsole');
 var http = require('http');
 var httpProxy = require('http-proxy');
 var request = require('request');
-var configuration = require('./configuration')
+var configuration = require('./configuration');
 
 var proxy = httpProxy.createProxyServer({
   target: configuration.proxyTarget
@@ -16,14 +16,14 @@ http.createServer(function(req, res) {
       body += chunk;
     });
     res.on('end', function(){
-      assemblyUrl = JSON.parse(body).assembly_url;
-      console.debug("received proxy response, polling assemblyUrl: %s", assemblyUrl)
+      var assemblyUrl = JSON.parse(body).assembly_url;
+      console.debug("received proxy response, polling assemblyUrl: %s", assemblyUrl);
       pollAssembly(assemblyUrl);
     });
   });
 }).listen(configuration.proxyPort);
 
-console.debug("listening on %d, forwarding to %s, notifying %s", configuration.proxyPort, configuration.proxyTarget, configuration.notifyUrl)
+console.debug("listening on %d, forwarding to %s, notifying %s", configuration.proxyPort, configuration.proxyTarget, configuration.notifyUrl);
 
 function pollAssembly(assemblyUrl, triesLeft){
   if(triesLeft < 1){
@@ -41,7 +41,7 @@ function pollAssembly(assemblyUrl, triesLeft){
         pollAssembly(assemblyUrl, triesLeft);
       }
       else{
-        console.debug("%s valid response, notifying.", assemblyUrl)
+        console.debug("%s valid response, notifying.", assemblyUrl);
         notify(response);
       }
     });
@@ -53,11 +53,11 @@ function checkAssembly(assemblyUrl, callback){
     var response = JSON.parse(body);
     if(response.ok){
       if(response.ok == 'ASSEMBLY_COMPLETED'){
-        console.debug('%s completed.', assemblyUrl)
+        console.debug('%s completed.', assemblyUrl);
         callback(null, response);
       }
       if(response.ok == 'ASSEMBLY_EXECUTING'){
-        console.debug('%s still executing.', assemblyUrl)
+        console.debug('%s still executing.', assemblyUrl);
         callback(null);
       }
     }
